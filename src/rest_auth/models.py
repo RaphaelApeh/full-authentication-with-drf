8b230e755e72967af6610952a00240be54459a82
raphael apeh <raphaelapeh37@gmail.com>
2025-01-12 22:48:52 +0100

    10 Added token and is_expired field to EmailConfirmtion model

--- src/rest_auth/models.py
+++ src/rest_auth/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -6,6 +8,8 @@ User = get_user_model()
 class EmailConfirmation(models.Model):
     user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
     email = models.EmailField(unique=True)
+    token = models.CharField(max_length=100, default=uuid.uuid4)
+    is_expired = models.BooleanField(default=False)
     is_confirmed = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
