c4790da7e61b7060d7a8cd9fcbbd44c991e284e9
raphael apeh <raphaelapeh37@gmail.com>
2025-12-25 21:12:05 +0100

    Added test

--- src/tests/test_social_account.py
+++ src/tests/test_social_account.py
@@ -3,7 +3,7 @@ from unittest import mock
 from rest_framework.test import APIRequestFactory
 
 from .base import TestCase
-from social_account.views.base import OauthLoginAPIView
+from social_account.views.base import OauthLoginAPIView, BaseOauthCallbackAPIView
 from social_account.models import SocialProvider
 
 
@@ -28,3 +28,27 @@ class SocailTestCase(TestCase):
         response = view(request)
         self.assertStatusCode(response)
         assert "https://example.com/authorize?response_type=code&client_id=abc" in response.data["authentication_url"]
+
+
+    def test_oauth_user_fetch_view(self):
+
+        request = self.factory.get("/")
+        request_mock = mock.MagicMock()
+        client_mock = mock.MagicMock()
+        request.session = {}
+        request_mock.json.return_value = {
+            "username": "johndoe", 
+            "email": "johndoe@test.com"
+        }
+        client_mock.access_token.return_value = "token"
+        request_mock.raise_for_status.return_value = None
+        view = BaseOauthCallbackAPIView()
+        view.request = request
+        view.redirect_url="https://example.com/redirect",
+        view.userinfo_url="https://example.com/user",
+        view.token_url = "https://example.com/token"
+        with mock.patch.object(view, "get_response", request_mock):
+            data = view._fetch_userinfo(request, client_mock)
+            self.assertIsNotNone(data)
+            assert request.session["username"] == "johndoe"
+        
